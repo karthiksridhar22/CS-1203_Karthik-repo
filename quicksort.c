2e7c578ebe003fd8c48ca68b889538894aa66fd6
karthiksridhar22 <karthik.sridhar_ug24@ashoka.edu.in>
2022-11-11 20:10:26 +0530

    quicksort for arrays

--- quicksort.c
+++ quicksort.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-int n = 10; 
+int n = 10; // size of linked list
 
 int* createArr(int n){
     int* arr = malloc(sizeof(int)*n); 
@@ -22,13 +22,51 @@ void swap(int* a, int* b){
     
 }
 
-int main(){
-    int * a = createArr(n);
+int partition(int* arr, int low, int high)
+{
 
-    for (int i =0; i < n; i++){
-        printf("%d ", a[i]);
+    int pivot_val = arr[high];
+    int i = low;
+    for (int j = low; j < high; j++){
+
+        if (arr[j] < pivot_val)
+        {
+            swap(&arr[j], &arr[i]);
+            i++; 
+        }
+
     }
+    
+    swap(&arr[i], &arr[high]); 
+return i; 
+}
 
+
+void quicksort_recursion(int* arr, int low, int high){
+
+    if (low < high)
+    {
+    int pivot = partition(arr, low, high); 
+    quicksort_recursion(arr, low, pivot - 1);
+    quicksort_recursion(arr, pivot + 1, high);
+    }
+}
+
+void quicksort(int* arr){
+    quicksort_recursion(arr, 0, n);
+
+}
+
+
+int main(){
+
+int* arr = createArr(n); 
+
+quicksort(arr); 
+
+for (int i = 0; i < n; i++){
+    printf( "%d ", arr[i]);
+}
 
     return 0;
 }
