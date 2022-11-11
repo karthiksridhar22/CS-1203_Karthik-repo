a0e7d8f220b23ed98d64ded8347d362540051d5e
karthiksridhar22 <karthik.sridhar_ug24@ashoka.edu.in>
2022-11-11 23:41:59 +0530

    quickSort linked (only step 1 working)

---
+++ quickLinked.c
@@ -0,0 +1,109 @@
+// quicksort for linked list
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+
+    int val;
+    struct node* next;
+
+};
+
+typedef struct node* NodeAddress;
+
+int a[] = {2, 7, 1, 9, 8, 4};
+int length = 6;
+
+
+void printList(struct node* n){
+
+    while (n != NULL){
+        printf("[%d]", n -> val);
+        n = n -> next; 
+    }
+}
+
+
+
+
+struct node* Arr2Link(int a[])
+{   
+    struct node * head; 
+    head = malloc(sizeof(struct node*));
+    head -> next = malloc(sizeof(struct node)); 
+        head -> val = a[0]; 
+        struct node* temp = head; 
+    for (int i = 1; i< length; i++){
+        
+        temp -> next = malloc(sizeof(struct node)); 
+        temp = temp -> next; 
+        temp -> val = a[i];
+    }
+    printList(head);
+    return head;
+
+}
+
+
+
+void swap(struct node* a, struct node* b)
+{
+int temp = a->val ; 
+a->val = b->val;
+b->val = temp; 
+}
+
+struct node* getTail(struct node* head)
+{
+    while (head->next != NULL)
+    {
+        head = head->next; 
+    }
+printf(" tail: %d ", head-> val);
+return head; 
+
+}
+struct node* partition(struct node* head, struct node* tail)
+{
+    struct node* prev = head;
+    struct node* pivot = tail; 
+    struct node* temp = head; 
+    while (temp != tail)
+    {
+        if (temp -> val < pivot -> val)
+        {
+            swap(temp, prev);  
+            prev = prev->next;   
+        }
+        temp = temp -> next; 
+    }
+    swap(pivot, prev); 
+
+    return pivot; 
+}
+
+void quicksortRecursion(struct node*head, struct node* tail)
+{   
+    if (head < tail)
+    {
+        struct node* pivot = partition(head, tail);
+        partition(head, pivot); 
+        partition(pivot, tail);
+    }
+}
+
+void quickSortLink(struct node* head)
+{   
+    struct node* tail = getTail(head); 
+    quicksortRecursion(head, tail); 
+
+}
+
+int main(){
+    struct node* head = Arr2Link(a);
+    quickSortLink(head);
+    printList(head);
+    return 0;
+}
