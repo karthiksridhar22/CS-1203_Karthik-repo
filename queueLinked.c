57c246f29abe32c5c424a823132cc071d29e52a0
karthiksridhar22 <karthik.sridhar_ug24@ashoka.edu.in>
2022-12-23 18:18:35 +0530

    edits

---
+++ queueLinked.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+//queue linkedlist
+
+struct node{
+    int val;
+    struct node* next;
+};
+
+typedef struct node* nodeAddress;
+
+nodeAddress enqueue(int val){
+    nodeAddress temp = 
+}
